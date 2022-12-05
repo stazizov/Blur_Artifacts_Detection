426027ce7b06c5a76cb3ea12452781a1f3b22f37
Said Azizov <69383296+proton-bit@users.noreply.github.com>
2022-12-05 11:35:32 +0300

    preparation for train YOLOv7_640_640

--- yolo_preprocessing.py
+++ yolo_preprocessing.py
@@ -2,37 +2,30 @@ import cv2
 import numpy as np
 import albumentations as A
 from math import ceil
+import os
 
-image_filename = "WSS1/train/images/crop_1_2.png"
-annotation_filename = "WSS1/train/labels/crop_1_2.xml"
-image = cv2.imread(image_filename)
-window_size = 640
+# image_filename = "WSS1/train/images/crop_1_2.png"
+# annotation_filename = "WSS1/train/labels/crop_1_2.xml"
+# image = cv2.imread(image_filename)
+# window_size = 640
 
 
-preprocessing = A.Compose([A.augmentations.crops.transforms.RandomSizedCrop(
-    [window_size, window_size], 
-    window_size, window_size
-    )
-])
+# preprocessing = A.Compose([A.augmentations.crops.transforms.RandomSizedCrop(
+#     [window_size, window_size], 
+#     window_size, window_size
+#     )
+# ])
 
-image = preprocessing(image=image)["image"]
-cv2.imshow('image', image)
-cv2.waitKey(0)
+# image = preprocessing(image=image)["image"]
+# cv2.imshow('image', image)
+# cv2.waitKey(0)
 
-# print(image.shape)
-# for y_index in range(ceil(image.shape[0] / window_size)):
-#     y_top = y_index * window_size
-#     y_bottom = (y_index + 1) * window_size
-#     delta_y = image.shape[0] - y_bottom
-#     if delta_y < 0:
-#         y_top -= abs(delta_y)
-#         y_bottom -= abs(delta_y)
-        
-#     for x_index in range(ceil(image.shape[1] / window_size)):
-#         x_left = x_index * window_size
-#         x_right = (x_index + 1) * window_size
-#         delta_x = image.shape[1] - x_right
-#         if delta_x < 0:
-#             x_left -= abs(delta_x)
-#             x_right -= abs(delta_x)
-#         print(y_top, y_bottom, x_left, x_right)
+for images_dir in ["train", "test"]:
+    images_dir = f"WSS1_640_640/{images_dir}/images"
+    for filename in os.listdir(images_dir):
+        if filename.startswith("."):
+            continue
+        filepath = os.path.join(images_dir, filename)
+        image = cv2.imread(filepath)
+        image = cv2.resize(image, (640, 640))
+        cv2.imwrite(filepath, image)

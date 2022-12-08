c1031d34dc97d1243a5bf7d898f76d5d959da03d
Said Azizov <69383296+proton-bit@users.noreply.github.com>
2022-12-09 00:25:53 +0300

    Co-authored-by: Alexey Zemtsov <zzmtsvv@users.noreply.github.com>

--- data_preparation/generate_patches.py
+++ data_preparation/generate_patches.py
@@ -1,4 +1,3 @@
-from sliding_window import sliding_window
 import cv2
 import os 
 
@@ -11,5 +10,12 @@ if __name__ == '__main__':
 
     image = cv2.imread("01.png")
     window_size = 128
+
+    if not os.path.exists(f"patches_{window_size}"):
+      os.mkdir(f"patches_{window_size}")
     for x, y, patch in sliding_window(image, window_size, (window_size, window_size)):
-        cv2.imwrite(os.path.join(f"patches_{window_size}", f"patch_{x//window_size}_{y // window_size}.png"), patch)
+        dst_path = os.path.join(
+          f"patches_{window_size}", 
+          f"patch_{x//window_size}_{y // window_size}.png"
+          )
+        cv2.imwrite(dst_path, patch)
